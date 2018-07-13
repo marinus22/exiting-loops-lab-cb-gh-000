4efabb920411b4b62b221de6e25283deb9895267
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-13 14:15:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
     if (array[i] == findFn()) {
         return array[i];
-        break;
     } else if (i+1 == array.length) {
         return null;
     } else {
