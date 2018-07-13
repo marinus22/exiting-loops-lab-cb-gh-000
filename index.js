1f96b531d9a2699342675a8844ff8dff5bb976a4
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-13 13:38:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function breakOut(array, changeValue, stopValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue;
+    } else {
+        break
+    }
+  }
+    return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i < array.length; i ++) {
+    if (array[i] !== skipValue) {
+      array[i] = changeValue;
+    } else {
+        continue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  const x = findFn();
+  var t = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == x) {
+      break;
+    } else {
+      t++;
+    }
+  }
+  if (t == i) {
+    return null;
+  }
+}
